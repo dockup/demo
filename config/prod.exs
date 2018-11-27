23ea5b0b24cc32985df6e2ab685aef3c087f44ab
akash <pomalakash@gmail.com>
2018-11-27 15:06:27 +0530

    start server 🤦‍♂️

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dockup_demo, DockupDemoWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  load_from_system_env: true,
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: "."
 
 # Do not print debug messages in production
 config :logger, level: :info
